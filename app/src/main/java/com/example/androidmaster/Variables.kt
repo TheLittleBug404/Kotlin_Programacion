7f14522e7dd3e6fa78b629db4cbef59f767768e6
Jauregui <ricardodamiancitoje@gmail.com>
2023-05-26 20:27:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/androidmaster/Variables.kt
@@ -0,0 +1,12 @@
+package com.example.androidmaster
+//Variables
+fun main(){
+    /**
+     * VARIABLES BOOLEANAS
+     */
+    //Boolean
+    val booleanExample:Boolean = true
+    val booleanExample1:Boolean = false
+    var inmutableExample = 14
+    inmutableExample = 15
+}
