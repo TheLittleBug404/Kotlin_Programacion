7090feaf10f999e4d5856282b21037c4b71dc2bd
Jauregui <ricardodamiancitoje@gmail.com>
2023-06-28 22:01:26 -0400

    Aca agregamos concatenacion y cambio de un tipo de variable a otro

---
+++ app/src/main/java/com/example/androidmaster/CambiioDeUnTipoDeVariableAOtro.kt
@@ -0,0 +1,20 @@
+package com.example.androidmaster
+
+//en aca veremos como podemos cambiar de uns tipo de variable a otro tipo de variable
+//para convertir de una variable a otra simplemente usamos el toInt,toFloat,toString etc
+//tambien veremos la concatenacion de cadenas
+fun IntAFloat(a:Int, b:Float){
+   println("Cambiamos de un tipo Int a un Float")
+   var suma:Int = a + b.toInt()
+   println(suma)
+}
+fun concatenarString(a:String, b:String){
+    println(a+b)
+    //recoredemos que en kotlin la concatenacion es con el simbolo de $ (dolar)
+    var nuevoString:String = "Esta es la nueva palabra de tip String $a y $b"
+    println(nuevoString)
+}
+fun main(){
+    IntAFloat(5,6.5f)
+    concatenarString("Ricardo","fernando")
+}
--- app/src/main/java/com/example/androidmaster/OperacionesAritmeticas.kt
+++ app/src/main/java/com/example/androidmaster/OperacionesAritmeticas.kt
@@ -1,5 +1,33 @@
 package com.example.androidmaster
-
+//estas son simplemente funciones para kotlin como las de la suma resta etc
+fun suma(){
+    val age:Int = 32
+    var age2:Int = 33
+    println("La suma sera: ")
+    println(age+age2)
+}
+fun restar(){
+    var valor1 = 4
+    var valor2 = 2
+    println("la resta sera: ")
+    println(valor1-valor2)
+}
+fun multiplicacion(a:Int, b:Int){
+    println("La multiplicacion sera: ")
+    println(a*b)
+}
+fun division(a:Int, b:Int){
+    println("La division sera: ")
+    println(a/b)
+}
+fun modulo(a:Double, b:Double){
+    println("El modulo de la division sera de: ")
+    println(a%b)
+}
 fun main(){
-
+    suma()
+    restar()
+    multiplicacion(5,6)
+    division(15,3)
+    modulo(16.0,3.0)
 }
